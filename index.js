44a76100f40fa3c9dfd33a3de785871880d7f23e
KayAreBe <kjkoenaite@gmail.com>
2023-06-07 09:49:43 +0200

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const app = express()
 const port = 3000
 const db = require('./queries')
@@ -10,6 +12,46 @@ app.use(
     extended: true,
   })
 )
+// Implement authentication middleware to protect routes
+function authenticate(req, res, next) {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    res.status(401).json({ error: 'Unauthorized' });
+    return;
+  }
+
+  try {
+    // Verify the JWT token
+    const decodedToken = jwt.verify(token, 'your_secret_key');
+
+    // Attach the user ID to the request for further use
+    req.userId = decodedToken.userId;
+
+    next();
+  } catch (error) {
+    res.status(401).json({ error: 'Unauthorized' });
+  }
+}
+
+app.get('/protected', authenticate, (req, res) => {
+  // Access the authenticated user's ID using req.userId
+  res.json({ message: 'This is a protected route' });
+});
+
+// middleware function for role-based access control
+function authorize(roles) {
+  return (req, res, next) => {
+    const { role } = req.user;
+
+    if (!roles.includes(role)) {
+      res.status(403).json({ error: 'Forbidden' });
+      return;
+    }
+
+    next();
+  };
+}
 
 app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' })
@@ -20,6 +62,13 @@ app.get('/users/:id', db.getUserById)
 app.post('/users', db.createUser)
 app.put('/users/:id', db.updateUser)
 app.delete('/users/:id', db.deleteUser)
+
+app.get('/admin', db.getAdmin)
+app.post('/admin', db.createAdmin)
+app.put('/admin/:id', db.updateAdmin)
+app.delete('/admin/:id', db.deleteAdmin)
+
+app.post('/user/login', db.login)
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
--- queries.js
+++ queries.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const Pool = require('pg').Pool
 const pool = new Pool({
   user: 'postgres',
@@ -16,6 +18,15 @@ const getUsers = (request, response) => {
     })
 }
 
+const getAdmin = (request, response) => {
+  pool.query('SELECT * FROM admin ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
 const getUserById = (request, response) => {
     const id = parseInt(request.params.id)
   
@@ -27,15 +38,31 @@ const getUserById = (request, response) => {
     })
 }
 
-const createUser = (request, response) => {
-    const { name, email } = request.body
+const createUser = async (request, response) => {
+    const { name, email, password } = request.body
+
+    
+
+     // Hash the password
+     const hashedPassword = await bcrypt.hash(password, 10);
   
-    pool.query('INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *', [name, email], (error, results) => {
+    pool.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *', [name, email, hashedPassword], (error, results) => {
       if (error) {
         throw error
       }
       response.status(201).send(`User added with ID: ${results.rows[0].id}`)
     })
+}
+
+const createAdmin = (request, response) => {
+  const { username, email, password } = request.body
+
+  pool.query('INSERT INTO admin (username, email, password) VALUES ($1, $2, $3) RETURNING *', [username, email, password], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).send(`Admin added with ID: ${results.rows[0].id}`)
+  })
 }
 
 const updateUser = (request, response) => {
@@ -54,6 +81,22 @@ const updateUser = (request, response) => {
     )
 }
 
+const updateAdmin = (request, response) => {
+  const id = parseInt(request.params.id)
+  const { username, email, password} = request.body
+
+  pool.query(
+    'UPDATE admin SET username = $1, email = $2, password = $3 WHERE id = $4',
+    [username, email, password, id],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`User modified with ID: ${id}`)
+    }
+  )
+}
+
 const deleteUser = (request, response) => {
     const id = parseInt(request.params.id)
   
@@ -65,10 +108,64 @@ const deleteUser = (request, response) => {
     })
 }
 
+const deleteAdmin = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('DELETE FROM admin WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).send(`Admin deleted with ID: ${id}`)
+  })
+}
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Find the user by username in the database
+    const query = 'SELECT * FROM users WHERE email = $1';
+    const values = [email];
+
+    const result = await pool.query(query, values);
+
+    if (result.rows.length === 0) {
+      res.status(401).json({ error: 'Authentication failed' });
+      return;
+    }
+
+    // Verify the password
+    const user = result.rows[0];
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      res.status(401).json({ error: 'Authentication failed' });
+      return;
+    }
+
+    // Generate a JWT token
+    const token = jwt.sign({ userId: user.id }, 'your_secret_key');
+
+    // Return the token to the client
+    res.json({ token });
+    if(token){
+      console.log('User logged in');
+    }
+  } catch (error) {
+    console.error('Error creating token:', error);
+    res.status(500).json({ error: 'Error creating token' });
+  }
+}
+
 module.exports = {
     getUsers,
     getUserById,
     createUser,
     updateUser,
     deleteUser,
+    getAdmin,
+    createAdmin,
+    updateAdmin,
+    deleteAdmin,
+    login
 }
